4bdb03541a85ae69b7aca8a4df5842af7d006ecf
ZaytsevAndrey <Adryathic@gmail.com>
2022-11-21 23:23:28 +0100

    Update Modal.js

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Modal = (props) => {
+// @ todo add propTypes
+
+const Modal = ({ onHideModal }) => {
     return (
         <div className="modal_container">
             <h2 className="modal_title">Quizzical</h2>
             <h3 className="modal_descr">Play intelligent game, test your knowledge!</h3>
-            <button className="modal_btn" onClick={props.onHideModal}>Start quiz</button>
+            <button className="modal_btn" onClick={ onHideModal }>Start quiz</button>
 
         </div>
     )
